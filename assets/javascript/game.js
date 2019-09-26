599ff8b25ad9015afd8c1555b283e94644bfafea
Chris Davies <c.m.davies10@gmail.com>
2019-09-26 15:10:16 -0400

    working version of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,9 @@
-// set up baseline values
 var winScore= 0;
 var lossScore = 0;
 var guessStart = 10;
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-//=============================================
-// set up functions
+var guesses = [];
+    
 document.onkeyup = function (event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -15,18 +13,27 @@ document.onkeyup = function (event) {
         guessStart = 10
     } else {
         guessStart--;
-    }
+    };
 
     if (guessStart === 0) {
         guessStart = 10;
         lossScore++;
     } else {
-    }
+    };
+
+    // create an array for each "life" that stores each keystroke or guess within each life
+    // lookup mozzila - array methods -- help to push items into array
+    guesses.push(userGuess);
+    console.log(guesses);
 
 
-    // display text 
+    // for loop that pushes user guess up to 10 into the guesses array
+    // for (var i = 0; i < 10; i++) {
+        // guesses.push(userGuess);
+    // };
+   
     document.getElementById("wins-text").innerHTML = "Wins: " + winScore;
     document.getElementById("losses-text").innerHTML = "Losses: " + lossScore;
     document.getElementById("guessesLeft-text").innerHTML = "Guesses Left: " + guessStart;
-    document.getElementById("guesses-text").innerHTML = "Your Guesses Thus Far: " + userGuess;
+    document.getElementById("guesses-text").innerHTML = "Your Guesses Thus Far: " + guesses;
 };
