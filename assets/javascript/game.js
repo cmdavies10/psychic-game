60bce827f6e1dda84a72139eaef0df4c1b0c0567
Chris Davies <c.m.davies10@gmail.com>
2019-09-26 17:11:15 -0400

    updated with fully-functioning game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,34 +3,26 @@ var lossScore = 0;
 var guessStart = 10;
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guesses = [];
-    
+
 document.onkeyup = function (event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
+    guesses.push(userGuess);
+
     if (userGuess === computerGuess) {
         winScore++;
-        guessStart = 10
+        guessStart = 10;
+        guesses.length = 0;
     } else {
-        guessStart--;
+        guessStart--; 
     };
 
     if (guessStart === 0) {
         guessStart = 10;
         lossScore++;
+        guesses.length = 0;
     } else {
     };
-
-    // create an array for each "life" that stores each keystroke or guess within each life
-    // lookup mozzila - array methods -- help to push items into array
-    guesses.push(userGuess);
-    console.log(guesses);
-
-
-    // for loop that pushes user guess up to 10 into the guesses array
-    // for (var i = 0; i < 10; i++) {
-        // guesses.push(userGuess);
-    // };
    
     document.getElementById("wins-text").innerHTML = "Wins: " + winScore;
     document.getElementById("losses-text").innerHTML = "Losses: " + lossScore;
